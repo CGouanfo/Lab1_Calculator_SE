dc46dc94d789454f6355905f55b83983dcbd4d8c
CGouanfo <gouanfocynthia@gmail.com>
2017-01-24 18:37:22 +0000

    implemented the add function

---
+++ Calc.java
@@ -0,0 +1,12 @@
+public class Calc{
+
+public static void main(String[] args){
+
+if(args[0].equalsIgnoreCase("add")){
+double one = Double.parseDouble(args[1]);
+double two = Double.parseDouble(args[2]);
+System.out.println(one+two);
+
+}
+}
+}
