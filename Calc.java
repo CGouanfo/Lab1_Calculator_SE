5bcebc425318ec56b507a6a32e535760803cf449
Job mwesigwa <Job mwesigwa>
2017-01-24 22:38:35 +0000

    Master changes merged

--- Calc.java
+++ Calc.java
@@ -1,26 +1,33 @@
 import java.util.Scanner;
 public class Calc{
-
-public static void main(String[] args){
-
-if(args[0].equalsIgnoreCase("add")){
-double one = Double.parseDouble(args[1]);
-double two = Double.parseDouble(args[2]);
-System.out.println(one+two);
-}
-
-
-else if(args[0].equalsIgnoreCase("subtract")){
-double one = Double.parseDouble(args[1]);
-double two = Double.parseDouble(args[2]);
-System.out.println(one-two);
-
-if(args[0].equalsIgnoreCase("multiply")){
-double one = Double.parseDouble(args[1]);
-double two = Double.parseDouble(args[2]);
-System.out.println(one*two);
-
-}
-
-}
+  
+  public static void main(String[] args){
+    
+    if(args[0].equalsIgnoreCase("add")){
+      double one = Double.parseDouble(args[1]);
+      double two = Double.parseDouble(args[2]);
+      System.out.println(one+two);
+    }
+    
+    
+    else if(args[0].equalsIgnoreCase("subtract")){
+      double one = Double.parseDouble(args[1]);
+      double two = Double.parseDouble(args[2]);
+      System.out.println(one-two);
+    }
+    
+    if(args[0].equalsIgnoreCase("multiply")){
+      double one = Double.parseDouble(args[1]);
+      double two = Double.parseDouble(args[2]);
+      System.out.println(one*two);
+      
+    }
+    
+    if(args[0].equalsIgnoreCase("devide")){
+      double one = Double.parseDouble(args[1]);
+      double two = Double.parseDouble(args[2]);
+      System.out.println(one/two);
+      
+    }
+  }
 }
